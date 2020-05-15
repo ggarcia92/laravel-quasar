7a422edd75559e431bc620c57f2f180267b30298
Gustavo <ggarciagonzalez92@gmail.com>
2020-05-15 14:26:53 -0400

    Fix colors template

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,21 @@ import router from './router'
 import App from "./app.vue"
 
 Vue.use(Quasar, {
-    lang: langQ
+    lang: langQ,
+    config: {
+        brand: {
+            primary: '#B48BE3',
+            secondary: '#7fada9',
+            accent: '#c9a7cf',
+
+            dark: '#1d1d1d',
+
+            positive: '#21BA45',
+            negative: '#C10015',
+            info: '#31CCEC',
+            warning: '#F2C037'
+        }
+    }
 });
 
 
